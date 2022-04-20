f9970b2bcd1190053b4430fa8974c19b6c58fba0
bperez <borjapgl@gmail.com>
2022-04-20 16:27:12 +0200

    agregar dependencias style dictionary

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Wed, 20 Apr 2022 14:26:36 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
